593f1e54f2b8953a8528cbaf818dfb0d92c0a630
Murilo Eduardo <murilo@vnda.com.br>
2018-09-19 14:06:23 -0300

    @ngrx/store - @ngrx/store-devtools

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,60 @@
 import { Component, TemplateRef } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Post } from './models/post.model';
+import * as PostActions from './actions/post.actions';
+
+interface AppState {
+  post: Post;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public modalRef: BsModalRef; // {1}
-  constructor(private modalService: BsModalService) { } // {2}
+
+  public post$: Observable<Post>;
+  public modalRef: BsModalRef;
+
+  public text: string;
+
+  constructor(
+    private modalService: BsModalService,
+    private store: Store<AppState>
+  ) {
+    this.post$ = this.store.select('post');
+  }
+
+  public editText() {
+    this.store.dispatch(new PostActions.EditText(this.text));
+  }
+
+  public upvote() {
+    this.store.dispatch(new PostActions.Upvote());
+  }
+
+  public downvote() {
+    this.store.dispatch(new PostActions.Downvote());
+  }
+
+  public resetPost() {
+    this.store.dispatch(new PostActions.Reset());
+  }
+
+  public spanishMessage() {
+    this.store.dispatch({ type: 'SPANISH' });
+  }
+
+  public frenchMessage() {
+    this.store.dispatch({ type: 'FRENCH' });
+  }
 
   public openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template); // {3}
+    this.modalRef = this.modalService.show(template);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
@@ -8,18 +12,29 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { simpleReducer } from './reducers/simple.reducer';
+import { postReducer } from './reducers/post.reducer';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BsDropdownModule.forRoot(),
-    TooltipModule.forRoot(),
-    ModalModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        BsDropdownModule.forRoot(),
+        TooltipModule.forRoot(),
+        ModalModule.forRoot(),
+        StoreModule.forRoot({
+            post: postReducer,
+            message: simpleReducer
+        }),
+        StoreDevtoolsModule.instrument({
+            maxAge: 10
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
